188454da42a7a84f48927ee9968099b6d3502a71
Austen Ploudre <60715092+austenploudre@users.noreply.github.com>
2021-09-30 16:16:42 -0700

    Updated uptime calculation and linux_parser.cpp

--- CppND-System-Monitor/src/processor.cpp
+++
@@ -1,9 +0,0 @@
-#include "processor.h"
-#include "linux_parser.h"
-
-// Return the aggregate CPU utilization
-float Processor::Utilization() {
-  long active = LinuxParser::ActiveJiffies();
-  long total = LinuxParser::Jiffies();
-  return active * (1.f / total);
-}
